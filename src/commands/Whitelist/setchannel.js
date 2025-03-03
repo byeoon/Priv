5163829544022eaeda28b917577c6b30f1021d3d
vorlie <vorlie0961@gmail.com>
2025-03-03 16:07:33 +0100

    feat: add whitelist channel configuration and update command

--- src/commands/Whitelist/setchannel.js
+++ src/commands/Whitelist/setchannel.js
@@ -1,24 +1,42 @@
-const { SlashCommandBuilder, PermissionFlagsBits  } = require('discord.js');
-const { ChannelType, Client, Events, Formatters, GatewayIntentBits, PermissionsBitField } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+
+const configPath = path.resolve(__dirname, '../../config.json');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('whitelist setchannel')
-		.setDescription('Set the channel to output whitelist logs to.')
+    data: new SlashCommandBuilder()
+        .setName('whitelist-setchannel')
+        .setDescription('Set the channel to output whitelist logs to.')
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
         .setDMPermission(false)
         .addChannelOption(option =>
-		option.setName('channel')
-			.setDescription('The channel to set as the default whitelist channel.')
+        option.setName('channel')
+            .setDescription('The channel to set as the default whitelist channel.')
             .setRequired(true)),
-	async execute(interaction) {
-       try {
-        const selectedchannel = interaction.options.getChannel("channel");
-        console.log("[Protector] Will now send whitelist logs to " + selectedchannel.id);
-            selectedchannel.permissionOverwrites.edit(interaction.guildId, { ViewChannel: false }).then(interaction.reply(":hammer: Successfully locked down the channel!"));
+    async execute(interaction) {
+        try {
+            const selectedchannel = interaction.options.getChannel("channel");
+            console.log("[Protector] Will now send whitelist logs to " + selectedchannel.id);
+
+            // Ensure the config file exists
+            if (!fs.existsSync(configPath)) {
+                fs.writeFileSync(configPath, JSON.stringify({}));
+            }
+
+            // Read the existing config
+            const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+
+            // Update the config with the new channel ID
+            config.whitelistChannelId = selectedchannel.id;
+
+            // Save the updated config
+            fs.writeFileSync(configPath, JSON.stringify(config, null, 2));
+
+            await interaction.reply(":white_check_mark: Successfully set the whitelist log channel!");
+        } catch (error) {
+            console.error(error);
+            await interaction.reply(':x: There was an error setting the default channel.');
         }
-        catch {
-            await interaction.reply(':x: There was an error setting the default chanel.');
-        } 
-	},  
+    },
 };
