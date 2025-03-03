5b5e58bf8f107aa942afdff6ae5a4d6f641a7a12
byeoon <47872200+byeoon@users.noreply.github.com>
2025-03-03 15:13:44 +0000

    oops leaked our token

--- src/index.js
+++ src/index.js
@@ -22,8 +22,6 @@ for (const folder of commandFolders) {
 	}
 }
 
-
-
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 	const command = interaction.client.commands.get(interaction.commandName);
